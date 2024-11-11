5cf541153ea6a12370f1920008ecd52ef92a425d
brandon mazey <brandon@brandons-MacBook-Air.local>
2024-11-11 12:26:43 -0500

    template

---
+++ src/main/java/Game.java
@@ -0,0 +1,23 @@
+package main.java;
+
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {
+
+        // Create Scanner for collecting user input.
+        Scanner scanner = new Scanner(System.in);
+
+        // Collect character name from user.
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+
+        // TODO Create Tav instance
+        // TODO Implement generateMonster()
+        // TODO Collect buffs from user
+        // TODO Implement roll() method for Tav
+        // TODO Print Critical Success & Critical Failure
+        // TODO Check for 3 successful monster encounters
+        // TODO Print GAME OVER or winning message 
+    }
+}
---
+++ src/main/java/Tav.java
@@ -0,0 +1,21 @@
+package main.java;
+
+import java.util.Random;
+
+public class Tav {
+    
+    // TODO - create Tav class here!
+
+    // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    private void shuffle(int[] array) {
+        Random random = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            int index = random.nextInt(i + 1);
+            
+            // Swap elements
+            int temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+        }
+    }
+}
